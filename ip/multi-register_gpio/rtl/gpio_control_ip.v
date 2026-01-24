be00dfedd35b27ab6cabbe2629672c8424c737aa
Tejas Patel <no.95.free@gmail.com>
2026-01-24 16:05:23 +0530

    Parameterize GPIO control IP for flexible widths

--- ip/multi-register_gpio/rtl/gpio_control_ip.v
+++ ip/multi-register_gpio/rtl/gpio_control_ip.v
@@ -1,14 +1,18 @@
-module gpio_control_ip (
-    input             clk,
-    input             resetn,
+module gpio_control_ip #(
+    parameter ADDR_WIDTH = 4,
+    parameter DATA_WIDTH = 32,
+    parameter GPIO_WIDTH = 4
+)(
+    input                       clk,
+    input                       resetn,
     // Bus Interface
-    input             i_sel,      // Chip Select
-    input             i_we,       // Write Enable
-    input      [3:0]  i_addr,     // Address Offset
-    input      [31:0] i_wdata,    // Data from CPU
-    output reg [31:0] o_rdata,    // Data to CPU (Readback)
+    input                       i_sel,      // Chip Select
+    input                       i_we,       // Write Enable
+    input      [ADDR_WIDTH-1:0] i_addr,     // Address Offset
+    input      [DATA_WIDTH-1:0] i_wdata,    // Data from CPU
+    output reg [DATA_WIDTH-1:0] o_rdata,    // Data to CPU (Readback)
     // External Interface
-    inout      [3:0]  gpio_pins   // Bidirectional GPIO pins
+    inout      [GPIO_WIDTH-1:0] gpio_pins   // Bidirectional GPIO pins
 );
 
     // Register Map Offsets
@@ -17,18 +21,18 @@ module gpio_control_ip (
     localparam READ = 4'h8;
 
     // Registers
-    reg [3:0] gpio_data;
-    reg [3:0] gpio_dir;
+    reg [GPIO_WIDTH-1:0] gpio_data;
+    reg [GPIO_WIDTH-1:0] gpio_dir;
 
     // --- Write Logic ---
-    always @(posedge clk) begin
+    always @(posedge clk or negedge resetn) begin
         if (!resetn) begin
-            gpio_data <= 32'b0;
-            gpio_dir  <= 32'b0;
+            gpio_data <= 0;
+            gpio_dir  <= 0;
         end else if (i_sel && i_we) begin
             case (i_addr)
-                DATA: gpio_data <= i_wdata[3:0];
-                DIR : gpio_dir  <= i_wdata[3:0];
+                DATA: gpio_data <= i_wdata[GPIO_WIDTH-1:0];
+                DIR : gpio_dir  <= i_wdata[GPIO_WIDTH-1:0];
             endcase
         end
     end
@@ -37,10 +41,10 @@ module gpio_control_ip (
     always @(*) begin
         if (i_sel && !i_we) begin
             case (i_addr)
-                DATA   : o_rdata = gpio_data;          // Read data
-                DIR    : o_rdata = gpio_dir;           // Read direction
-                READ   : o_rdata = {28'b0, gpio_pins}; // Read actual pin state
-                default: o_rdata = 32'b0;
+                DATA   : o_rdata = gpio_data;                                    // Read data
+                DIR    : o_rdata = gpio_dir;                                     // Read direction
+                READ   : o_rdata = {{(DATA_WIDTH-GPIO_WIDTH){1'b0}}, gpio_pins}; // Read actual pin state
+                default: o_rdata = 0;
             endcase
         end
     end
@@ -49,7 +53,7 @@ module gpio_control_ip (
     // 1 (Output mode), 0 (Input mode)
     genvar i;
     generate
-        for (i = 0; i < 4; i = i + 1) begin : gpio_ctrl
+        for (i = 0; i < GPIO_WIDTH; i = i + 1) begin : gpio_ctrl
             assign gpio_pins[i] = gpio_dir[i] ? gpio_data[i] : 1'bz;
         end
     endgenerate
